c81b928c75844cd5070c467a0dc5a671bbc2a2e6
nstrucky <nickstruckmeyer@gmail.com>
2017-01-25 17:43:11 -0500

    Added PetContract and inner PetEntry class

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,33 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+import android.provider.ContactsContract;
+
+/**
+ * Created by root on 1/25/17.
+ */
+
+public final class PetContract {
+
+
+    private PetContract() {}
+
+
+    public static class PetEntry implements BaseColumns {
+
+        private PetEntry() {}
+
+        public static final String TABLE_NAME = "pets";
+        public static final String COLUMN_NAME_ID = BaseColumns._ID;
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_BREED = "breed";
+        public static final String COLUMN_NAME_GENDER = "gender";
+        public static final String COLUMN_NAME_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_FEMALE = 1;
+        public static final int GENDER_MALE = 2;
+
+
+    }
+}
